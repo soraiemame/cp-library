cf819be7ae5d40ce90edfcd2cb2311e70145ab63
soraiemame <funasou46@yahoo.co.jp>
2021-07-25 11:36:57 +0900

    add workflow

---
+++ dijkstra.cpp
@@ -0,0 +1,12 @@
+priority_queue<pll,vector<pll>,greater<pll>> que;
+vector<ll> dist(N);
+que.emplace(0,0);
+dist[0] = 0;
+while(!que.empty()){
+    auto p = que.top();
+    que.pop();
+    if(dist[p.second] < p.first)continue;
+    for(auto x : G[p.second]){
+        if(chmin(dist[x.first],dist[p.second] + x.second))que.emplace(dist[x.first],x.first);
+    }
+}
---
+++ gen.py
@@ -0,0 +1,24 @@
+# generate snippet codes
+
+import glob
+import json
+
+def main():
+    paths = glob.glob("*.cpp")
+    res = {}
+
+    for path in paths:
+        with open(path) as file:
+            fname = path.replace(".cpp","")
+            res[fname] = { "prefix": f"@{fname}" }
+            body = []
+            for line in file:
+                body.append(line.strip("\n"))
+            res[fname]["body"] = body
+            res[fname]["description"] = fname
+    
+    with open("snippet.json","w") as out:
+        json.dump(res,out,indent=4)
+
+if __name__ == "__main__":
+    main()
